5c3e82d60f4f2fbfbe9906570e4aaeee7b0ffd90
Sunil Kumar <sunilbitaan@gmail.com>
2025-04-17 08:50:03 +0300

    First commit as setup of project - Add koin and hilt libraries to use koi for DI.

---
+++ app/src/main/java/com/example/accenturecodeinterview/AndroidApplication.kt
@@ -0,0 +1,13 @@
+package com.example.accenturecodeinterview
+
+/**
+ * Create By Sunil Kumar
+ */
+import android.app.Application
+
+class AndroidApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
